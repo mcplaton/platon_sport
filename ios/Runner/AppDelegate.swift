9ad1c2c9a93ea9e485a35b9a670feb22adfc90f9
Mc Platon - افلاطون <98595150+mcplaton@users.noreply.github.com>
2025-09-05 18:03:38 +0300

    AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,27 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Firebase آمن: لا ينهار إذا ملف GoogleService-Info.plist موجود
+    if let _ = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
+      FirebaseApp.configure()
+    }
+
     GeneratedPluginRegistrant.register(with: self)
+
+    // إصلاح مشاكل WebView/وسائط بالخلفية
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+    UIApplication.shared.isIdleTimerDisabled = true
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
